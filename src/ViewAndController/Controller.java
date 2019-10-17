de1918b7d6ef4fc4032a4ee36b99fc13d18242a3
Ulysses Chen <1027910051@qq.com>
2019-10-17 19:29:12 +0800

    把功能加到示例界面里

--- src/ViewAndController/Controller.java
+++ src/ViewAndController/Controller.java
@@ -1,14 +1,20 @@
 package ViewAndController;
 
+import Parser.Parser;
+import Parser.FourYuan;
+import Parser.LexicalParser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.stage.FileChooser;
 
+import java.awt.*;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.String;
+import java.util.List;
 
 public class Controller {
     @FXML
@@ -36,26 +42,61 @@ public class Controller {
         if(text==null||"".equals(text.trim())){
 
         }else{
+            Parser parse = new Parser();///////分析实例
+            LexicalParser lexicalParser = new LexicalParser();
+            lexicalParser.setSourceCode(input.getText());
+            //使用getAllTokens()方法获取Tokens,返回一个包含了识别出的Tokens的ArrayList
+            List<String> tokens1 = lexicalParser.getAllTokens();
+            for (String token: tokens1) {
+                parse.tokens.add(token);
+            }
+            parse.token = parse.tokens.get(parse.cur++);////读入第一个单词
+            parse.L();
+            ////测试
+            System.out.println("算术表达式栈顶的存放位置："+parse.Es.peek().des);
+            System.out.println("识别出算术表达式的数量："+parse.symbols.size());
+            System.out.println("算术表达式状态栈栈顶：（正确时应该为1）"+parse.states.peek());
+            System.out.println("栈顶逻辑表达式需要回填的真出口链:");
+            for(int i=0;i<parse.Bs.peek().truelist.size();i++) {
+                System.out.print(parse.Bs.peek().truelist.get(i)+"   ");
+            }
+            System.out.println("");
+            System.out.println("需要回填的假出口链：");
+            for(int i=0;i<parse.Bs.peek().falselist.size();i++) {
+                System.out.print(parse.Bs.peek().falselist.get(i)+"   ");
+            }
+            System.out.println("");
+            System.out.println(parse.Bs.size());
+            System.out.println("token:"+parse.token);
+            for(int i=0;i<parse.fours.size();i++) {
+                System.out.print(i+":   ");
+                parse.fours.get(i).print();
+            }
+            System.out.println("下一条指令地址："+ FourYuan.no);
             //todo 将需要输出的内容输出到output中
             output.setText("输出内容");
         }
     }
 
+    private static Frame frame;
     public void onActionOpenFile(ActionEvent actionEvent){
-        //todo 这是打开文件的按钮，可以不需要
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("TXT file (*.txt)","*.txt");
-        fileChooser.getExtensionFilters().add(extensionFilter);
-        File file = fileChooser.showOpenDialog(main.getPrimaryStage());
-        if(file!=null){
-            //todo print the
-            try{
-                final int FILESIZE = 1024;
-                char[] buf = new char[FILESIZE];
-                FileReader fr = new FileReader(file);
-                int len = fr.read(buf);
-                output.setText(new String(buf));
-            }catch (IOException e){e.printStackTrace();}
+        FileDialog fileDialog = new FileDialog(frame,"test file",FileDialog.LOAD);
+        fileDialog.setVisible(true);
+        String str_file = fileDialog.getDirectory() + fileDialog.getFile();
+        File file = new File(str_file);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            while (reader.ready()) {
+                builder.append(reader.readLine());
+                builder.append('\n');
+            }
+            String sourceCode = builder.toString();
+            input.setText(sourceCode);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
         }
     }
 }
